d95f9bf35a1924deb1f4d1a6e4336b5dde97e61c
Kanya Paramita <kanyaparamita@yahoo.com>
2020-05-27 14:48:28 +0200

    Add some Kanya reference

--- js/refClick.js
+++ js/refClick.js
@@ -16,7 +16,16 @@ const refs = {
     'ramasubbu2007' : 'Ramasubbu, N. & Balan, R. (2007). Globally distributed software development project performance: An empirical analysis. 125-134.',
     'espinosa2003' : 'Espinosa, J., & Carmel, E. (2003). Modeling Coordination Costs Due to Time Separation in Global Software Teams.',
     'vesey1992' : 'Vesey, J. T. (1992). Time-to-market: Put speed in product development. Industrial Marketing Management, 21(2), 151-158.',
-    'yapnd' : 'Yap, M. (n.d.). Follow the sun: distributed extreme programming development. Agile Development Conference (ADC’05).'
+    'yapnd' : 'Yap, M. (n.d.). Follow the sun: distributed extreme programming development. Agile Development Conference (ADC’05).',
+    'jvesey1992' : 'Joseph T. Vesey (1992). Time-to-market: Put speed in product developmentIndustrial Marketing Management, 21(2), 151 - 158.',
+    'reinersten1995' : 'Smith, P.G., & Reinertsen, D.G. (1995). Developing Products In Half The Time. New York: Van Nostrand Reinhold.',
+    'gupta2009' : 'Gupta, Amar. (2009). The 24-Hour Knowledge Factory: Can It Replace the Graveyard Shift?. Computer. 42. 66 - 73. 10.1109/MC.2009.25.',
+    'acameron2004' : 'A. Cameron (2004), ”A Novel Approach to Distributed Concurrent Software Development using a “Follow-the- Sun” Technique”.',
+    'carmel1999' : 'Carmel, Erran. (1999). Global Software Teams: Collaborating Across Borders and Time Zones. Upper Saddle River, NJ : Prentice Hall-PTR.',
+    'espinosa2007' : 'Espinosa, J. & Nan, Ning & Carmel, Erran. (2007). Do Gradations of Time Zone Separation Make a Difference in Performance? A First Laboratory Study. Proceedings - International Conference on Global Software Engineering, ICGSE 2007. 12-22. 10.1109/ICGSE.2007.20.',
+    'cummings2009' : 'Cummings, J., Espinosa, J., & Pickering, C. (2009). Crossing Spatial and Temporal Boundaries in Globally Distributed Projects: A Relational Model of Coordination Delay. Information Systems Research. 20. 420-439. 10.1287/isre.1090.0239.',
+    'godinez2007' : 'Godinez, V (2007). Sunshine 24/7: As EDS work stops in one time zone, it picks up in another. Dallas Morning News. Accessed January 2, 2007.',
+    'bweek2003' : 'Business Week (2003), The Rise of India. Published December 8, 2003.'
 }
 
 $(function () {
